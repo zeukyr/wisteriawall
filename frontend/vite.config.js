4008fe692d94d29ade350448da698b742f787a40
zeukyr <angelayxli@gmail.com>
2025-12-20 14:37:23 -0500

    adjusted vite for vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: '.',
   plugins: [
     tailwindcss(),
     react()],
