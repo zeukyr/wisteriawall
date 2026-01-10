303c8e571c666b582335aaf7d6db320f67e42393
zeukyr <angelayxli@gmail.com>
2026-01-10 12:19:23 -0500

    your posts section

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -15,8 +15,9 @@ const LandingPage = ({ }) => {
       >
             <div className="flex flex-col gap-4 items-center justify-center h-screen gap-1">
             <h1 className="text-4xl font-bold text-center text-gray-800 mb-3 tracking-tight">
-                    Wisteria Wall
+                    Eunoia
                 </h1>
+                <h2 className="text-lg text-center text-gray-600 mb-6 px-4"> An anonymous message board for women in CS </h2>
                 <div className="flex flex-row gap-4 w-72"> 
                     <button onClick={() => navigate("/login")}
                         className="w-full bg-linear-to-r from-purple-500 to-purple-700 text-white font-semibold py-4 px-6 rounded-xl shadow-lg hover:shadow-xl hover:from-purple-600 hover:to-purple-800 transform hover:-translate-y-0.5 transition-all duration-200"> Log In </button> 
